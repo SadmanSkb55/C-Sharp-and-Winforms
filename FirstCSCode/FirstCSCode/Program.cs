83be378635025e54e461a01913f5966d8b80e0f0
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-04-04 14:14:59 +0600

    First CSharp Code

---
+++ FirstCSCode/FirstCSCode/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter A text:");
+        string userInput = Console.ReadLine();
+        Console.WriteLine(userInput);
+    }
+}
