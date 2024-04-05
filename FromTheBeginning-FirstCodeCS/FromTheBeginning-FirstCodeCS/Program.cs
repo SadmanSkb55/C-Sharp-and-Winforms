21027dd614f744e2f76c0854beddcac7e286850e
SadmanSkb55 <119743258+SadmanSkb55@users.noreply.github.com>
2024-04-05 23:56:42 +0600

    CS second tryout

---
+++ FromTheBeginning-FirstCodeCS/FromTheBeginning-FirstCodeCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FromTheBeginning_FirstCodeCS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ayooooo....wtf nega....where did u got those nuggets dyauumnnn");
+            Console.Beep();
+            Console.Write("Yoooooo madapakha");
+            Console.WriteLine("Wassuppp");
+            Console.WriteLine("\nWhat da hell u doin der\br");
+            Console.WriteLine("\aAyooo...Ntg\f...jus....");
+            Console.WriteLine("haha\r...gotcha\v...gotcha\t....so...ummm");
+            //Console.WriteLine("what\?");
+            Console.WriteLine("\'What\'....\"dyauummm\"\\fuckske");
+            Console.ReadKey();
+
+        }
+    }
+}
